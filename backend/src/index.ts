f32cfc6313de387f9b6097f4bd711993129d1419
pvpkiselev <pvpkiselev@gmail.com>
2025-03-07 19:43:56 +0300

    trpc, eslint, prettier, husky

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello from backend!')
+import * as trpcExpress from '@trpc/server/adapters/express'
+import cors from 'cors'
+import express from 'express'
+import { trpcRouter } from './trpc'
+
+const expressApp = express()
+expressApp.use(cors())
+expressApp.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }))
+
+expressApp.listen(3000, () => {
+  console.info('listening at http://localhost:3000')
+})
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,10 +1,9 @@
-import './index.css'
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
