eea96d04cd489d5561c05bdf9d4d62b8f4581a64
pvpkiselev <pvpkiselev@gmail.com>
2025-03-15 17:27:23 +0300

    Postresql, prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,28 @@
 import cors from 'cors'
 import express from 'express'
+import { type AppContext, createAppContext } from './lib/ctx'
 import { applyTrpcToExpressApp } from './lib/trpc'
 import { trpcRouter } from './router'
 
-const expressApp = express()
-expressApp.use(cors())
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
+void (async () => {
+  let ctx: AppContext | null = null
 
-applyTrpcToExpressApp(expressApp, trpcRouter)
+  try {
+    ctx = createAppContext()
 
-expressApp.listen(3000, () => {
-  console.info('listening at http://localhost:3000')
-})
+    const expressApp = express()
+    expressApp.use(cors())
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
+
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter)
+
+    expressApp.listen(3000, () => {
+      console.info('listening at http://localhost:3000')
+    })
+  } catch (error) {
+    console.error(error)
+    await ctx?.stop()
+  }
+})()
